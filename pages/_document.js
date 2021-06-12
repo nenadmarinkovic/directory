09d537c17c9c7c6e57a30c2bdf698318b2278c65
Nenad Marinkovic <nenadmarinkovic@protonmail.com>
2021-06-12 12:47:29 +0200

    google fonts testing speed

--- pages/_document.js
+++ pages/_document.js
@@ -38,7 +38,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter"
+          rel="stylesheet"
+        />
+        </Head>
         <body>
           <Main />
           <NextScript />
--- styles/global.js
+++ styles/global.js
@@ -16,7 +16,7 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   html {
-    font-family:  -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: "Inter", -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-size: 16px;
     background: white;
   }
